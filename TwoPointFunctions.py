d504a9ec454e71090b353c10f65f7de8e7ce3d90
Pietro Butti <pietro.butti.fl@gmail.com>
2023-12-28 17:30:02 +0100

    Reading of correlators added. Ready for analysis

--- TwoPointFunctions.py
+++ TwoPointFunctions.py
@@ -8,13 +8,14 @@ import matplotlib.pyplot as plt
 import lsqfit
 import os
 import sys
+import h5py
 from autograd import numpy as np
 
 import FnalHISQMetadata
 
 
 
-ENSEMBLE_LIST = ['MediumCoarse', 'Coarse-2', 'Coarse-1', 'Coarse-Phys', 'Fine-1', 'SuperFine']#'Fine-Phys', 'SuperFine'] 
+ENSEMBLE_LIST = ['MediumCoarse', 'Coarse-2', 'Coarse-1', 'Coarse-Phys', 'Fine-1', 'SuperFine', 'Fine-Phys', 'SuperFine'] 
 MESON_LIST    = ["Dsst", "Bs", "D", "Ds", "Dst", "Dsst", "K", "pi", "Bc", "Bst", "Bsst"]
 MOMENTUM_LIST = ["000", "100", "200", "300", "400", "110", "211", "222"]
 
@@ -42,21 +43,35 @@ def NplusN2ptModel(Nstates,Nt,sm,pol):
     return _
 
 
+def jkCorr(C,bsize):
+    Nb  = C.shape[0]//bsize
+    rem = C.shape[0]%bsize==0
 
+    Cjk = []
+    for k in range(Nb):
+        Cjk.append(C[:Nb*bsize,:].reshape((Nb,bsize,C.shape[1])).mean(axis=1)[np.delete(range(Nb),k),:].mean(axis=0))
+
+    return np.array(Cjk)
 
 
 
 class CorrelatorInfo:
-    def __new__(cls, _name:str, _ens:str, _mes:str, _mom:str):
-        if _ens not in ENSEMBLE_LIST: 
-            raise KeyError(f'{_ens} is not a valid ensemble.')
-        if _mes not in MESON_LIST: # Check whether meson is recognized
-            raise KeyError(f'{_mes} is not a valid meson.')
-        if _mom not in MOMENTUM_LIST: # Check whether momentum is recognized
-            raise KeyError(f'{_mom} is not a valid momentum.')
-        return super().__new__(cls)
-        # if _file is not None and not os.path.isfile(_file): # Check whether input file exist
-        #     raise RuntimeError(f'{_file} has not been found.')
+    """
+        This is a basic structure to contain information for 2pt functions for the following given attributes:
+        - ensemble
+        - meson
+        - momentum
+    """
+    # def __new__(cls, _name:str, _ens:str, _mes:str, _mom:str):
+    #     if _ens not in ENSEMBLE_LIST: 
+    #         raise KeyError(f'{_ens} is not a valid ensemble.')
+    #     if _mes not in MESON_LIST: # Check whether meson is recognized
+    #         raise KeyError(f'{_mes} is not a valid meson.')
+    #     if _mom not in MOMENTUM_LIST: # Check whether momentum is recognized
+    #         raise KeyError(f'{_mom} is not a valid momentum.')
+    #     return super().__new__(cls)
+    #     # if _file is not None and not os.path.isfile(_file): # Check whether input file exist
+    #     #     raise RuntimeError(f'{_file} has not been found.')
 
     def __init__(self, _name:str, _ens:str, _mes:str, _mom:str):
         self.name     = _name
@@ -69,9 +84,14 @@ class CorrelatorInfo:
     def __str__(self):
         return f' # ------------- {self.name} -------------\n # ensemble = {self.ensemble}\n #    meson = {self.meson}\n # momentum = {self.momentum}\n #  binsize = {self.binsize}\n # filename = {self.filename}\n # ---------------------------------------'
 
-
-
 class CorrelatorIO:
+    """
+        This is the basic structure that deals with input/output of the correlator data.
+
+        Data are read from a typical hdf5 archive, e.g. `l3264f211b600m00507m0507m628.hdf5` that can be provided by the additional argument `PathToFile`.
+        
+        If no other additional argument are specified, the location of the hdf5 archive is *automatically* inferred from `FnalHISQMetadata`. The location of the ensemble folder *must* be specified with `PathToDataDir`.
+    """
     def __init__(self, _ens:str, _mes:str, _mom:str, PathToFile=None, PathToDataDir=None, name=None):
         dname = f'{_ens}_{_mes}_p{_mom}' if name is None else name
 
@@ -88,16 +108,18 @@ class CorrelatorIO:
                 raise FileNotFoundError(f'The file {path} has not been found')
         else:
             raise FileNotFoundError(f'Please specify as optional argument either data file in `PathToFile` or `PathToDataDir` to look at default data file.')
-        
+
+    def checkSmear(self, myStr):
+        return '1S' if '1S' in myStr else 'rot' if 'rot' in myStr else 'd'
+
     def CorrFileList(self, sms=None):
-        # if moms is None:
-        #     moms = self.mData['moms']
+        # Set informations for reading
         if sms is None:
             sms = self.mData['sms']
-
         meson = self.info.meson
         mom   = self.info.momentum
 
+        # Infer information for file extraction
         B       = meson.startswith('B')
         D       = meson.startswith('D')
 
@@ -109,50 +131,140 @@ class CorrelatorIO:
         heavy   = f'k{self.mData["kBStr"]}' if B else (f'k{self.mData["kDStr"]}' if D else None)
         light   = f'k{self.mData["kDStr"]}' if charm else (f'm{self.mData["msStr"]}' if strange else f'm{self.mData["mlStr"]}')
 
-        clist = []
-        for lss in sms:
-            for hss in sms:
-                # for mm in moms:
-                if meson=='K':
-                    clist.append(f'P5_P5_{lss}_{hss}_m{self.mData["msStr"]}_m{self.mData["mlStr"]}_p{mom}')
-                    continue
-                elif meson=='pi':
-                    clist.append(f'P5_P5_{lss}_{hss}_m{self.mData["mlStr"]}_m{self.mData["mlStr"]}_p{mom}')
-                else:
-                    for ch in channel:
-                        clist.append(f'{ch}_{lss}_{hss}_{heavy}_{light}_p{mom}')
+        # Read data from master file
+        with h5py.File(self.CorrFile,'r') as d:
+            data = d['data']
 
-        # self.CorrFileList = clist
-        return clist
+            # Cycle over all possible smearing, build filename and check if 
+            clist = []
+            for lss in sms:
+                for hss in sms:
+                    if meson=='K':
+                        newfiles = [f'P5_P5_{lss}_{hss}_m{self.mData["msStr"]}_m{self.mData["mlStr"]}_p{mom}']
+                        continue
+                    elif meson=='pi':
+                        newfiles = [f'P5_P5_{lss}_{hss}_m{self.mData["mlStr"]}_m{self.mData["mlStr"]}_p{mom}']
+                    else:
+                        newfiles = [f'{ch}_{lss}_{hss}_{heavy}_{light}_p{mom}' for ch in channel]
+                    
+                    aux = [f for f in newfiles if f in data]
+                    
+                    smr_string = f'{self.checkSmear(lss)}_{self.checkSmear(hss)}'
+                    clist.append((aux,smr_string)) if aux else 1
 
+            return clist
 
+    def ReadCorrelator(self,sms=None):
+        CorrList = self.CorrFileList(sms=sms)
+        T  = self.mData['T']//2
 
+        with h5py.File(self.CorrFile,'r') as d:
+            data = d['data']
+
+            allCorrs = []
+            allSmear = []
+            allPolar = set()
+            for clist,smr in CorrList:
+                aux = []
+                for corr in clist:
+                    Ci_t = np.array(data[corr])
+                    Ci_t = 0.5*( # Fold correlators
+                        Ci_t[:,:T] + 
+                        np.flip( np.roll(Ci_t,-1,axis=1) ,axis=1)[:,:T]
+                    ) 
+                    aux.append(Ci_t)
+                
+                if len(aux)==1: # scalar correlator
+                    allSmear.append(smr)
+                    allPolar.add('Unpol')
+                    allCorrs.append(aux)
+                
+                if len(aux)==3: # vector correlator
+                    crPar = aux[0]*0.0
+                    crBot = aux[0]*0.0
+
+                    mom = self.info.momentum
+                    if mom[1:]=='00' and mom!='000':
+                        nBot = 0
+                        for j,p in enumerate(mom):
+                            pj = int(p)
+                            crPar += aux[j]*pj
+                            if pj==0:
+                                crBot += aux[j]
+                                nBot += 1
+                        crPar /= np.array([int(x) for x in mom]).sum()
+
+                        allSmear.append(smr)
+                        allPolar.add(('Par','Bot'))
+                        allCorrs.append([crPar,crBot])
+
+                    else:
+                        allSmear.append(smr)
+                        allPolar.add('Unpol')
+                        allCorrs.append([np.array(aux).mean(axis=0)])
+
+        DATA = np.array(allCorrs)
+        xd = xr.DataArray(
+            DATA,
+            dims   = ['smearing','polarization','jkbin','timeslice'],
+            coords = [allSmear,list(*allPolar),np.arange(DATA.shape[-2]),np.arange(DATA.shape[-1])],
+            attrs  = {
+                'ensemble': self.info.ensemble,
+                'meson'   : self.info.meson,
+                'momenutm': mom,
+                'list'    : CorrList,
+            },
+            name   = self.info.name
+        )
+        return xd
 
 class Correlator:
     """
         This is the basic structure for a correlator at fixed momentum. It contains data for all possible smearings and polarization.
     """
-    def __init__(self, INPUT, format='pickle'):
-        if format=='pickle':
-            if os.path.isfile(INPUT):
-                self.filename = INPUT
-                with open(INPUT,'rb') as handle:
-                    self.data = pickle.load(handle)
-        elif format=='xarray':
-            self.data = INPUT
-            self.filename = None
+    def __init__(self, io:CorrelatorIO, sms=None):
+        self.data = io.ReadCorrelator(sms=sms)
+        self.Nt   = 2*self.data.timeslice.size
+        self.info = io.info  
 
-        self.info = CorrelatorInfo(
-            self.data.name,
-            self.data.attrs['ensemble'],
-            self.data.attrs['meson'],
-            self.data.attrs['momentum'],
-            self.data.attrs['binsize'],
-            self.filename,
+    def jack(self, bsize):
+        self.info.binsize = bsize
+
+        newshape = (
+            self.data.shape[0],
+            self.data.shape[1],
+            self.data.shape[2]//bsize,
+            self.data.shape[3],
         )
-    
-        self.Nt = 2*self.data.timeslice.size
-        print(self.info)
+        MT = np.empty(newshape)
+
+        for ism,sm in enumerate(self.data.smearing):
+            for ipol,pol in enumerate(self.data.polarization):
+                MT[ism,ipol,:,:] = jkCorr(self.data.loc[sm,pol,:,:].values, bsize)
+
+        newcoords = [
+            self.data.smearing.values,
+            self.data.polarization.values,
+            np.arange(newshape[2]),
+            self.data.timeslice.values
+        ]
+        newattrs  = {
+            'ensemble': self.info.ensemble,
+            'meson'   : self.info.meson,
+            'momenutm': self.info.momentum,
+            'binsize' : bsize,
+            'list'    : self.data.attrs['list'],
+        }
+        xd = xr.DataArray(
+            MT,
+            dims   = ['smearing','polarization','jkbin','timeslice'],
+            coords = newcoords,
+            attrs  = newattrs, 
+            name   = self.info.name
+        )
+        self.data = xd
+        
+        return 0
 
     def __str__(self):
         return self.info
