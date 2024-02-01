e6650c81fd0e97d37b08ac66903e0ff9b5988867
Pietro Butti <pietro.butti.fl@gmail.com>
2024-02-01 16:34:07 +0100

    Added some reading of ratios

--- b2heavy/ThreePointFunctions/types3pts.py
+++ b2heavy/ThreePointFunctions/types3pts.py
@@ -1,4 +1,8 @@
 import os
+import h5py
+import gvar as gv
+import numpy as np
+import pandas as pd
 
 from .. import FnalHISQMetadata
 
@@ -37,14 +41,13 @@ class RatioIO:
         else:
             raise FileNotFoundError(f'Please specify as optional argument either data file in `PathToFile` or `PathToDataDir` to look at default data file.')
 
-
     def RatioSpecs(self):
         bStr    = '_k' + self.mData['kBStr']
         cStr    = '_k' + self.mData['kDStr']
         sStr    = '_m' + self.mData['msStr']
         mStr    = '_m' + self.mData['mlStr']
 
-        match self.ratio:
+        match self.info.ratio:
             case 'RA1':
                 hStr   = bStr
                 lStr   = cStr
@@ -153,12 +156,153 @@ class RatioIO:
             'dNames' : dNames
         }
 
-    def RatioFileName(self,sm):
-        specs = self.RatioSpecs()
-        return f'{self.ratio}'
+    def RatioFileString(self,name,ts,h,hss,q,l):
+        return f'{name}T{ts}{h}_RW_{hss}_rot_rot{q}{l}_p{self.info.momentum}'
 
+    def RatioFileDict(self,sms = ['RW','1S']):
+        specs = self.RatioSpecs()
+
+        aux = {}
+        for hss in sms:
+            aux[hss] = {}
+            for tSink in self.mData['hSinks']:
+                nuCorr = []
+                duCorr = []
+
+                flag = False
+                for j,(name,nFac) in enumerate(zip(specs['nNames'][0],specs['nFacs'][0])):
+                        # filename = f'{name}T{tSink}{specs["hStr"]}_RW_{hss}_rot_rot{specs["qStr"]}{specs["lStr"]}_p{self.info.momentum}'
+                        filename = self.RatioFileString(name,tSink,specs["hStr"],hss,specs["qStr"],specs["lStr"])
+                        nuCorr.append(filename)
+
+                if ('RA1' not in self.info.ratio) and (self.info.momentum!='000'):
+                    flag = True
+                    if len(specs['dNames'])>0:
+                        for j,name in enumerate(specs['dNames']):
+                            filename = self.RatioFileString(name,tSink,specs["hStr"],hss,specs["qStr"],specs["lStr"])
+                            duCorr.append(filename)
+
+                elif 'RA1' in self.info.ratio:
+                    flag = True
+                    target = specs['nNames'][0] if self.info.ratio!='ZRA1' else specs['nNames'][1]
+                    for j,name in enumerate(target):
+                        if self.info.ratio=='ZRA1':
+                            filename = self.RatioFileString(name,tSink,specs["lStr"],hss,specs["qStr"],specs["hStr"])
+                        else:
+                            filename = self.RatioFileString(name,tSink,specs["hStr"],hss,specs["qStr"],specs["lStr"])
+                        nuCorr.append(filename)
+
+                if flag:
+                    aux[hss][tSink] = dict(nu=nuCorr,du=duCorr)
+
+        return aux
+
+    def ReadRatio(self, sms = ['RW','1S']):
+        DATA  = h5py.File(self.RatioFile,'r')['data']
+        DATA2 = h5py.File(self.RatioFile,'r')['data']
+
+        specs = self.RatioSpecs()
+        T = self.mData['hSinks'][0]
+
+        if ('RA1' not in self.info.ratio) and (self.info.momentum!='000'):
+            FLAG_RA1 = True
+        elif 'RA1' in self.info.ratio:
+            FLAG_RA1 = False
+        else:
+            return 
+
+
+        PROCESSED = {}
+        for hss in sms:
+            if ('RA1' not in self.info.ratio) and (self.info.momentum!='000'):
+
+                AUX = []
+                for tSink in self.mData['hSinks']:
+                    nuCorr = []
+                    for j,(name,nFac) in enumerate(zip(specs['nNames'][0],specs['nFacs'][0])):
+                        corrname = self.RatioFileString(name,tSink,specs["hStr"],hss,specs["qStr"],specs["lStr"])
+                        try:
+                            nuCorr.append(DATA[corrname][:]*nFac)
+                        except:
+                            raise Exception('WARNING: ',corrname,' not found')
+                    nuCorr = np.mean(nuCorr,axis=0)
+
+                    duCorr = []
+                    if len(specs['dNames'])>0:
+                        for j,name in enumerate(specs['dNames']):
+                            corrname = self.RatioFileString(name,tSink,specs["hStr"],hss,specs["qStr"],specs["lStr"])
+                            duCorr.append(filename)
+
+                            try:
+                                duCorr.append(DATA[corrname])
+                            except KeyError:
+                                raise Exception('WARNING: ',corrname,' not found')
+
+                    AUX.append(
+                        nuCorr/np.mean(duCorr,axis=0) if duCorr else nuCorr
+                    )
+
+                PROCESSED[hss] = 0.5*AUX[0][:,0:T+1] + 0.25*AUX[1][:,0:T+1] + 0.25*np.roll(AUX[1], -1, axis=1)[:,0:T+1]
+
+
+            elif 'RA1' in self.info.ratio:
+                AUX = []
+                for tSink in self.mData['hSinks']:
+                    aux = []
+                    for name in specs['nNames'][0]:
+                        corrname = self.RatioFileString(name,tSink,specs["hStr"],hss,specs["qStr"],specs["lStr"])
+                        try:
+                            aux.append(DATA[corrname][:])
+                        except:
+                            raise Exception('WARNING: ',corrname,' not found')
+                    nuCorr = [np.array(aux)[:,:,0:(tSink+1)]]
+
+
+                    target = specs['nNames'][0] if self.info.ratio!='ZRA1' else specs['nNames'][1]
+                    aux = []
+                    for j,name in enumerate(target):
+                        if self.info.ratio=='ZRA1':
+                            corrname = self.RatioFileString(name,tSink,specs["lStr"],hss,specs["qStr"],specs["hStr"])
+                        else:
+                            corrname = self.RatioFileString(name,tSink,specs["hStr"],hss,specs["qStr"],specs["lStr"])
+                        try:
+                            aux.append(DATA[corrname][:])
+                        except:
+                            raise Exception('WARNING: ',corrname,' not found')
+
+                    aux = np.flip(np.array(aux)[:,:,0:(tSink+1)], axis=2) if self.info.ratio!='ZRA1' else np.array(aux)[:,:,0:(tSink+1)]
+                    nuCorr.append(aux)
+
+                    duCorr = []
+                    for mesStr, cName in zip([specs['lStr'],specs['hStr']],specs['dNames']):
+                        aux = []
+                        for name in enumerate(cName):
+                            corrname = self.RatioFileString(name,tSink,mesStr,hss,specs["qStr"],mesStr)
+                            try:
+                                aux.append(DATA[corrname][:])
+                            except:
+                                raise Exception('WARNING: ',corrname,' not found')
+                        aux = np.array(aux).mean(axis=0)[:,0:(tSink+1)]
+                        duCorr.append(aux)
+                    
+                    AUX.append(
+                        (nuCorr[0]*nuCorr[1]).sum(axis=0)/(duCorr[0]*duCorr[1])
+                    )
+                
+                PROCESSED[hss] = AUX
+
+
+
+
+
+
+        return PROCESSED
 
 def test():
-    io = RatioIO('Coarse-1','RA1','000',PathToDataDir='/Users/pietro/code/data_analysis/BtoD/Alex')
+    ra1 = RatioIO('Coarse-1',"RA1",'000',PathToDataDir='/Users/pietro/code/data_analysis/BtoD/Alex')
+    print(ra1.ReadRatio())
 
-    print(io.mData['hSinks'])
+
+    print('---------')
+    r0 = RatioIO('Coarse-1',"R0",'100',PathToDataDir='/Users/pietro/code/data_analysis/BtoD/Alex')
+    print(r0.ReadRatio())
